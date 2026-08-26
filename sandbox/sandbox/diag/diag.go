c4df49a88ced89976b5de91330b21045949eaa27
oink0x <251334198+oink0x@users.noreply.github.com>
2026-08-26 13:43:32 +0000

    sandbox: localize filesystem I/O helpers

--- sandbox/sandbox/diag/diag.go
+++ sandbox/sandbox/diag/diag.go
@@ -1,10 +1,12 @@
 package diag
 
 import (
+	"errors"
+	"fmt"
 	"log"
+	"os"
 	"syscall"
 
-	"sandbox/sandbox/fs"
 	"sandbox/sandbox/utils"
 )
 
@@ -20,14 +22,28 @@ func LogIdentity(label string, hostUID, hostGID int) {
 	log.Printf("[DEBUG] host: uid=%d,gid=%d", hostUID, hostGID)
 	log.Printf("[DEBUG] self: uid=%d,euid=%d,gid=%d,egid=%d", uid, euid, gid, egid)
 
-	uidMap := fs.ReadText("/proc/self/uid_map")
-	gidMap := fs.ReadText("/proc/self/gid_map")
+	uidMap := ReadText("/proc/self/uid_map")
+	gidMap := ReadText("/proc/self/gid_map")
 	log.Printf("[DEBUG] /proc/self/uid_map:\n%s", uidMap)
 	log.Printf("[DEBUG] /proc/self/gid_map:\n%s", gidMap)
 
-	statusText := fs.ReadText("/proc/self/status")
+	statusText := ReadText("/proc/self/status")
 	matchedLines := utils.SelectLinesWithPrefix(statusText, StatusKeys)
 	for _, line := range matchedLines {
 		log.Printf("[DEBUG] %s", line)
 	}
+}
+
+func ReadText(path string) string {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return "<missing>"
+		}
+		if errors.Is(err, os.ErrPermission) {
+			return fmt.Sprintf("<permission denied: %v>", err)
+		}
+		return fmt.Sprintf("<error: %v>", err)
+	}
+	return string(data)
 }
